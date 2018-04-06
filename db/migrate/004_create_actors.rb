9acbb19ed4bb27978d6258ff8311196d7f209073
Jawann <jawann@gmail.com>
2018-04-06 02:53:57 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,9 @@
-class AddFirstAndLastNameToActors < ActiveRecord::Migration[4.2]
+class CreateActors < ActiveRecord::Migration[4.2]
   def change
-    add_column :actors, :first_name, :string
-    add_column :actors, :last_name, :string
+    create_table :shows do |t|
+      #primary key of :id is created for us!
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
