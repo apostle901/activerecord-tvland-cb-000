345c7d227dfeb6abab44c5b904883297ebbd6eaa
Jawann <jawann@gmail.com>
2018-04-06 02:49:29 +0000

    automatically backed up by learn

--- db/migrate/004_add_first_and_last_name_to_actors.rb
+++ db/migrate/004_add_first_and_last_name_to_actors.rb
@@ -1,4 +1,4 @@
-class AddFirstAndLastNameToActor < ActiveRecord::Migration[4.2]
+class AddFirstAndLastNameToActors < ActiveRecord::Migration[4.2]
   def change
     add_column :actors, :first_name, :string
     add_column :actors, :last_name, :string
