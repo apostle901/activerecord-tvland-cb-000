a68e723f6882edbf00b2c30e02fa974e1f68939c
Jawann <jawann@gmail.com>
2018-04-06 02:56:36 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,9 @@
-class AddNameAndShowIdToCharacters < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :name, :string
-    add_column :characters, :show_id, :integer
+    create_table :characters do |t|
+      #primary key of :id is created for us!
+      t.string :name
+      t.integer :show_id
+    end
   end
 end
