e67cf3cdcdf0a535e9606a24b05483bead03309c
Jawann <jawann@gmail.com>
2018-04-06 02:24:01 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,6 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
 end
+
+Actor model with the Character and Show model. An actor should have many characters and
+many shows through characters.
