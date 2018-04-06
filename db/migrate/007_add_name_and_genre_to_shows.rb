31dc862f36643ab2d1f0bc8c29ee3bd4c1c9def2
Jawann <jawann@gmail.com>
2018-04-06 02:47:48 +0000

    automatically backed up by learn

--- db/migrate/007_add_name_and_genre_to_shows.rb
+++ db/migrate/007_add_name_and_genre_to_shows.rb
@@ -1,4 +1,4 @@
-class AddNameAndGenreToShows < ActiveRecord::Migration
+class AddNameAndGenreToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :name, :string
     add_column :shows, :genre, :string
